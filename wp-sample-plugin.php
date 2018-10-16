a32d9c7c1c70cd95c2b4b11e1947c1ed9c99380b
aramaki <reepn.bump@gmail.com>
2018-10-16 21:09:56 +0900

    最初のコミット

---
+++ wp-sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Sample Plugin
+Plugin URI: https://github.com/aramakiyurie/wp-sample-plugin
+Description: WordPress Plugin sample build.
+Version: 1.0.0
+Author: Yurie Aramaki
+Author URI: https://github.com/aramakiyurie/wp-sample-plugin
+License: GPLLv2 or later
+*/
